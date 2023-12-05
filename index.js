6becc9af2374ba71346ae57f10b34622d65dbc0a
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-05 23:50:32 +0530

    Alerts ,console log,comments
    it contains alert function,console log and comment

---
+++ index.js
@@ -0,0 +1,7 @@
+alert("i am learning js"); //text dialog box that pops up  on the screen
+        alert(4+8)
+        alert(2*89)
+        alert("please enter the valid information")
+
+        console.log("hello"); //static method that outputs a message
+        console.log("my name is vishnu")

18884cf35d59bfd813469f99fd030027c55707a8
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-06 00:31:28 +0530

    varibales,concatenation
    it contains a variable declaration and concatenation

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-alert("i am learning js"); //text dialog box that pops up  on the screen
+var msg="i am learnin js"
+
+alert(msg); //text dialog box that pops up  on the screen
         alert(4+8)
         alert(2*89)
-        alert("please enter the valid information")
+        alert(msg)
 
         console.log("hello"); //static method that outputs a message
         console.log("my name is vishnu")
+
+var name="vishnu";
+var message = "hello my name is" +  name  + "i am learning js";
+alert(message)
+
+
+let num1=20;
+let num2=30;
+
+let result= num1*num2;
+console.log(result)
+
+const n1=5;
+const n2=5;
+
+let final=5/5;
+console.log(final)

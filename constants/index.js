61e8735fd36c6437628c998992973d70c2406532
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-06 18:32:21 +0530

    Constants
    it has constant keyword

---
+++ constants/index.js
@@ -0,0 +1,24 @@
+let name ="john"
+name="john durai" //here we can reassign or modiy ley variable but not in const
+let msg=("hello my name is "+ name +  "I am learning js")
+console.log(msg)
+
+
+// const name ="john"
+// name="john durai" //it shows error as assignment to constant variable..
+// let msg=("hello my name is "+ name +  "I am learning js")
+// console.log(msg)
+
+const value=2.14
+
+let area=value*value
+console.log(area)
+
+const nam="vishnu"
+const dob="07/08/2003"
+
+let show=("my name is"+ nam +"and i born on " + dob + "i am learning js")
+
+console.log(show)
+
+//use constant if the value will not change...

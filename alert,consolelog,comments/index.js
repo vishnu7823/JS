78031fe80d1d260f1b9724b19383efaa8809d294
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-06 10:11:34 +0530

    alert(),console log,comments
    it contains alert function,console log and how to comment

---
+++ alert,consolelog,comments/index.js
@@ -0,0 +1,21 @@
+
+
+var name="vishnu";
+var message = "hello my name is" +  name  + "i am learning js";
+alert(message)
+
+
+let num1=20;
+let num2=30;
+
+let result= num1*num2;
+console.log(result)
+
+/*
+1.no js keywords name.
+2.should no start with number.
+3.No space and no -.
+4.case-sensitive.
+5.use meaning full names.
+
+

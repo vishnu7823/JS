e0add6aadf541d10bb2cbbee145a1e2025085e00
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-08 18:24:55 +0530

    Arrays
    it contains array declaration and finding  length function.

---
+++ arrays/index.js
@@ -0,0 +1,22 @@
+let favbikes = []//empty array
+console.log(favbikes);
+
+// {}--objects
+// []--arrays
+
+let favcars=["mustang","bmw","mercedes","porsche"] //length=4 starts from 0
+console.log(favcars);
+
+console.log(favcars[3]); //getting values of the array by giving index values
+console.log(favcars[0]);
+
+favcars[3]="audi"; //change values
+favcars[5]="fortuner" 
+console.log(favcars);
+
+//we can use datatypes in the arrays.
+
+let array=[1,2,3,4,5] //integer array
+console.log(array);
+
+console.log(favcars.length) //gives rhe latest length of the array.

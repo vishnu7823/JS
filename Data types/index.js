bacc725bf78841d44089063c34fd67f8b0d9f881
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-08 17:55:57 +0530

    Data types
    contains data types

---
+++ Data types/index.js
@@ -0,0 +1,26 @@
+let myname="vishnu123" //string
+
+let age= 20 //integer
+
+let isyoung = true //boolean
+
+let lastname=undefined //undefined
+
+let cup=null //null
+
+let x="car" + 16 //in this case js take 16 as string it reads from left to right
+
+console.log(x)
+
+
+let y=16+4+"bike" //in this case it takes 16 and 4 as number and performed the addition 
+console.log(y)
+
+// here js acts as dynamically typed language which means it take the types as its own by reading from left to right
+
+console.log(typeof(isyoung))
+console.log(typeof(myname))
+console.log(typeof(age))
+console.log(typeof(lastname))
+console.log(typeof(cup))  
+//typeof()  used to find out the data types.

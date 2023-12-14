67022d6a8a558208c30ba3eb24219f849f6ac801
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-14 22:13:24 +0530

    bitwise operators and order of precedence

---
+++ bitwise operators/index.js
@@ -0,0 +1,12 @@
+//human code - machine code(0,1)
+// 1 - 00000001
+
+console.log(1|2) //bitwise or
+console.log(1&2)//bitwise and
+
+//operator precedence
+let x= 10+5*25 //135 according  to precedence table
+console.log(x)
+
+let y=(10+5)*25
+console.log(y)

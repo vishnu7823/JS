4ffc23f6c3a540b5881f2185611fb4765af34875
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-15 23:42:21 +0530

    for loop

---
+++ for loop/index.js
@@ -0,0 +1,26 @@
+// js loops 
+// 1)for loop        4)for in loop
+// 2)while loop      5)for of loop
+// 3)do while loop     
+
+//syntax
+// for(initial expression;condition;step){
+//     statement
+// }
+
+
+for(let i=1;i<=50;i++)
+{
+    if(i%2!=0){
+        console.log("odd number"+i)
+   
+    }
+    else{
+        console.log("even number"+i)
+    }
+}
+
+for(let x=10;x>=0;x--)
+{
+    console.log(x)
+}

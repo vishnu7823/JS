7ed85bf910fa05015d2f9def5247b2630653c880
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2023-12-16 20:12:57 +0530

    while and do while loops

---
+++ while loop/index.js
@@ -0,0 +1,33 @@
+//while loop
+
+// while(condition){
+//     Statement;
+// }
+let i=10
+while(i>=1){
+    if(i%2==0){
+        console.log("even number"+i);
+    }
+
+    i--;
+}
+
+let val=1
+while(val<6){
+    console.log(val)
+    val+=1;
+}
+
+//do while
+// do{
+//     Statement
+// }while(condition)
+
+
+let x=10
+do{
+    if(x%2==0){
+        console.log("even number"+x);
+    }
+    x--;
+}while(x>=1)

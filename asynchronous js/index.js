091d59257e95269c733cc751291a0ccaa8f64c63
M Vishnu vardhan <122662900+vishnu7823@users.noreply.github.com>
2024-01-12 11:16:10 +0530

    asynchronous js

---
+++ asynchronous js/index.js
@@ -0,0 +1,12 @@
+
+console.log('step 1')
+console.log('step 2')
+console.log('step 3')
+
+setTimeout(()=>console.log('step1'),5000)
+setTimeout(()=>console.log('step2'),3000)
+setTimeout(()=>console.log('step3'),1000)
+
+console.log('bye')
+
+setInterval(()=> console.log("hello"),2000)
